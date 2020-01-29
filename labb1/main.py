4388a185f706d026d676c7e3e6b0f95b1ddc566f
potispenserr <rubcap-8@student.ltu.se>
2020-01-30 00:10:43 +0100

    he can mine chicken nuggs and die correctly

--- labb1/main.py
+++ labb1/main.py
@@ -1,17 +1,4 @@
-
-
-class State():
-
-    def Enter(self, miner):
-        print ("Enter the gungeon")
-        raise NotImplementedError("plizz ipmelemnt me :/")
-    def Execute(self, miner):
-        print("YOU HAVE BEEN EXECUTED")
-        raise NotImplementedError("plizz ipmelemnt me :/")
-
-    def Exit(self, miner):
-        print ("Escaped from tarkob")
-        raise NotImplementedError("plizz ipmelemnt me :/")
+import time
 
 class BaseGameEntity:
 
@@ -24,6 +11,83 @@ class BaseGameEntity:
     def update(self):
         raise NotImplementedError("pls implement me :(")
 
+
+
+
+class State():
+
+    def enter(self, miner):
+        print ("Enter the gungeon")
+        raise NotImplementedError("plizz ipmelemnt me :/")
+    def execute(self, miner):
+        print("YOU HAVE BEEN EXECUTED")
+        raise NotImplementedError("plizz ipmelemnt me :/")
+
+    def exit(self, miner):
+        print ("Escaped from tarkob")
+        raise NotImplementedError("plizz ipmelemnt me :/")
+
+
+class ImMiningForDiamonds(State):
+    def __new__(cls):
+        if not hasattr(cls, 'instance') or not cls.instance:
+            cls.instance = super().__new__(cls)
+        
+        return cls.instance
+
+    def enter(self, miner):
+        if(miner.location != "goldmine"):
+            print("wAlKinG tO tHE mINe")
+            miner.location = "goldmine"
+       
+    def execute(self, miner):
+        miner.nuggiesCarried +=1
+        miner.fatigue += 5
+        print("gettin' them Nugg Nuggz")
+
+        if(miner.nuggiesCarried >= miner.pocketsize):
+            miner.changeState(PushForceBank())
+        
+        if(miner.thirst > 50):
+            miner.changeState(GoToTravven())
+
+
+    def exit(self, miner):
+        print("brexit")
+
+
+
+
+class PushForceBank(State):
+
+    def enter(self, miner):
+        print("We want to hurt no one. We're here for the bank's money, not your money.")
+
+    def execute(self, miner):
+        print("Let's drill the safe")
+
+    def exit(self, miner):
+        print("Alright let's get the fuck outta here")
+
+    
+
+class GoToTravven(State):
+    pass
+
+
+class DanceTillYoureDead(State):
+    def enter(self, miner):
+        print("I don't feel so good Mr. Stark")
+
+    def execute(self, miner):
+        print("You lie dead on the floor")
+        miner.currentState = None
+
+    def exit(self, miner):
+        print("Guess who's back, back again")
+        
+
+
 class Miner(BaseGameEntity):
     def __init__(self,ID):
         super().__init__(ID)
@@ -31,34 +95,47 @@ class Miner(BaseGameEntity):
     currentState = State()
     location = ""
 
-    thirst = 0
+    thirst = 45
     hunger = 0
     fatigue = 0
     socialNeed = 0
 
     nuggiesCarried = 0
     nuggiesInThebank = 0
-    pocketsize = 2
+    pocketsize = 10
 
     def update(self):
         self.thirst += 1
-        print (self.thirst)
+        print ("thirst: ", self.thirst)
         if(self.currentState):
-            self.currentState.Execute(self)
+            if(self.thirst >= 50):
+                self.changeState(DanceTillYoureDead())
+            self.currentState.execute(self)
     
 
     def changeState(self, newState):
-        self.currentState.Exit()
+        print("changing state")
+        self.currentState.exit(self)
 
         self.currentState = newState
 
-        self.currentState.Enter()
+        self.currentState.enter(self)
+
+
 
 
 
 def main():
     miner = Miner(1)
-    miner.update()
+    miner.currentState = ImMiningForDiamonds()
+    #miner.update()
+    #miner.changeState(PushForceBank())
+    while(True):
+        miner.update()
+        time.sleep(0.5)
+        if(miner.currentState == None):
+            break
+    
 
 if __name__ == "__main__":
     main()
