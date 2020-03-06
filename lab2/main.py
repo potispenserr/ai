c77c7a5c0ac0498cf98733ee7dcdcf1ec6169f94
potispenserr <rubcap-8@student.ltu.se>
2020-03-06 18:11:44 +0100

    Mapparser complete
    TODO cleanup

--- lab2/main.py
+++ lab2/main.py
@@ -1,10 +1,102 @@
 import pygame
 import time
 
+class Level:
+    background = pygame.Surface((520, 460))
+    # starting positions
+    # startX is constant
+    startX = 50
+    # startY will increment by squareSize for each line
+    startY = 100
+
+    currentX = 50
+    currentY = 100
+
+    lineX = 50
+
+    def render(self):
+        f = open("Map1.txt", "r")
+        
+
+        squaresY = len(f.readlines())
+        f.close()
+
+        self.background.fill((255,255,255))
+
+        f = open("Map1.txt", "r")
+        for line in f:
+            # how many pixels a square has
+            squareSize = 10
+
+            self.lineX = self.startX
+            self.currentX = self.lineX
+            self.currentY = self.startY
+            for char in line:
+                if(char == "X"):
+                    for y in range(squareSize):
+                        for x in range(squareSize):
+                            self.background.set_at((self.currentX, self.currentY), (255, 0, 0, 0))
+                            self.currentX += 1
+                        self.currentX = self.lineX
+                        self.currentY += 1
+                        print("")
+                if(char == "S"):
+                    for y in range(squareSize):
+                        for x in range(squareSize):
+                            self.background.set_at((self.currentX, self.currentY), (0, 0, 255, 0))
+                            self.currentX += 1
+                        self.currentX = self.lineX
+                        self.currentY += 1
+                        print("")
+                if(char == "O"):
+                    for y in range(squareSize):
+                        for x in range(squareSize):
+                            self.background.set_at((self.currentX, self.currentY), (255, 255, 255, 0))
+                            self.currentX += 1
+                        self.currentX = self.lineX
+                        self.currentY += 1
+                        print("")
+                if(char == "G"):
+                    for y in range(squareSize):
+                        for x in range(squareSize):
+                            self.background.set_at((self.currentX, self.currentY), (0, 255, 0, 0))
+                            self.currentX += 1
+                        self.currentX = self.lineX
+                        self.currentY += 1
+                        print("")
+
+                self.currentY = self.startY
+                self.lineX += squareSize
+                self.currentX = self.lineX
+                print("\n")
+                #for char ends
+            
+            self.startX = 50
+            self.startY += squareSize
+        f.close()
+    
+    def drawSquare(self, obj):
+        if(obj == "wall"):
+            for y in range(squareSize):
+                for x in range(squareSize):
+                    self.background.set_at((self.currentX, self.currentY), (255, 0, 0, 0))
+                    self.currentX += 1
+                self.currentX = self.lineX
+                self.currentY += 1
+                print("")
+        elif(obj == "Goal"):
+            pass
+
+    def test(self):
+        image = pygame.image.load("1_img.png")
+        self.background.fill((255,255,255))
+        self.background.blit(image, (50,50))
+        print(self.background.get_at((50,50)))
+
 def main():
     # initialize the pygame module
     pygame.init()
-    
+
     # load and set the logo
     pygame.display.set_caption("first blit")
     screenWidth = 520
@@ -12,10 +104,10 @@ def main():
 
     # create a surface on screen that has the size of 240 x 180
     screen = pygame.display.set_mode((screenWidth,screenHeight))
-    
+
     # load image (it is in same directory)
-    image = pygame.image.load("01_image.png")
-    image.set_colorkey((255,0,255))
+    #image = pygame.image.load("01_image.png")
+    #image.set_colorkey((255,0,255))
 
     xpos = 50
     ypos = 50
@@ -23,67 +115,18 @@ def main():
     stepY = 10
 
     screen.fill((255,255,255))
-    
 
-    # blit image to screen
-    #screen.blit((image), (xpos,ypos))
-    f = open("Map1.txt", "r")
-    linelength = 0
-    startX = 50
-    startY = 100
+    level = Level()
+    level.render()
+    screen.blit(level.background, (0, 0))
 
-    currentX = 50
-    currentY = 100
-
-    squaresY = len(f.readlines())
-    f.close()
-
-    f = open("Map1.txt", "r")
-    for line in f:
-        print(line)
-        linelength = len(line.rstrip())
-        squaresX = int((screenWidth - startX * 2) / linelength)
-        squareSize = 26
-        for char in line:
-            print(char,"#")
-            if(char == "X"):
-                for y in range(squareSize):
-                    for x in range(squareSize):
-                        screen.set_at((currentX, currentY), (255, 0, 0, 0))
-                        print("drawing", char, "at", startX, startY)
-                        currentX += 1
-                        currentY += 1
-            if(char == "S"):
-                for x in range(squareSize):
-                    screen.set_at((startX, startY), (0, 0, 255, 0))
-                    startX += 1
-            if(char == "O"):
-                for x in range(squareSize):
-                    screen.set_at((startX, startY), (255, 255, 255, 0))
-                    startX += 1
-            if(char == "G"):
-                for x in range(squareSize):
-                    screen.set_at((startX, startY), (0, 255, 0, 0))
-                    startX += 1
-            currentY = startY
-        
-        print("changing line")
-        startX = 50
-        startY += 10
-
-    
-    f.close()
-
-    #for x in range(50):
-    #    screen.set_at((startX, startY), (255, 0, 0, 0))
-    #    startX += 1
 
     # update the screen to make the changes visible (fullscreen update)
     pygame.display.flip()
-    
+
     # define a variable to control the main loop
     running = True
-    
+
     # main loop
     while running:
         """ if(xpos > screenWidth - 64 or xpos <= 0):
